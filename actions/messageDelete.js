ea60b795f72df85176b3680cd22475bec8b4d976
Shaylin-8bit <malakai.8bit@outlook.com>
2022-07-27 12:51:34 -0500

    deleted logger now attempts to show who deleted

--- actions/messageDelete.js
+++ actions/messageDelete.js
@@ -1,15 +1,45 @@
 const { EmbedBuilder } = require('discord.js');
+const { AuditLogEvent } = require('discord.js');
 
-const genEmbed = (msg) => new EmbedBuilder() 
+const genEmbed = (msg, executor) => {
+    const title = `${msg.content ? 'Message' : 'Embed'} Deleted`;
+    const content = msg.content ? msg.content : `Title: ${msg.embeds[0].title}\nDescription:\n${msg.embeds[0].description}`;
+    
+    return new EmbedBuilder() 
 	.setColor(0x0099FF)
-	.setTitle(`Message deleted in ${msg.channel.name}`)
+	.setTitle(title)
 	.setURL(`https://discord.com/channels/${msg.guild.id}/${msg.channel.id}/${msg.id}`)
 	.setAuthor({ name: `${msg.author.username}#${msg.author.discriminator}`, iconURL: msg.author.displayAvatarURL()})
-	.setDescription(msg.content)
+	.setDescription(content)
+    .addFields(
+		{ name: 'Sender', value: `<@${msg.author.id}>`, inline: true},
+		{ name: 'Deleted by', value: `<@${executor}>`, inline: true },
+		{ name: 'Channel', value: `<#${msg.channel.id}>`, inline: true },
+	)
 	.setTimestamp()
+};
 
-const execute = (client, msg) => {
-    const embed = genEmbed(msg)
+const fetchDeleter = async (msg) => {
+    // the code in this function was borrowed from https://stackoverflow.com/questions/53328061/finding-who-deleted-the-message and modified
+    await setTimeout(() => null, 1000);
+    const fetchedLogs = await msg.guild.fetchAuditLogs({
+		limit: 8,
+		type: AuditLogEvent.MessageDelete,
+	});
+    
+    const auditEntry = fetchedLogs.entries.find(a =>
+        a.target.id === msg.author.id &&
+        a.extra.channel.id === msg.channel.id &&
+        Date.now() - a.createdTimestamp < 20000
+    );
+    const executor = auditEntry ? auditEntry.executor.id : msg.author.id;
+    return executor;
+}
+
+const execute = async (client, msg) => {
+    console.log('here');
+    const executor = await fetchDeleter(msg);
+    const embed = genEmbed(msg, executor)
     client.utilities.log({embeds: [embed]});
 }
 
--- docs/utilities.md
+++ docs/utilities.md
@@ -1,1 +1,10 @@
 A utility file should have the same name as it will be assigned under `client.utilities`. It should export an `execute` function, that accepts the `client` object, followed by what ever other arguments are needed.
+
+If used through `client.utilities`, `client` will automatically be passed to it through a wrapper function, and does not need to be passed in the call.
+
+Current utilities:
+
+utility    | args            | use
+-----------|-----------------|--------------------------------------------------------------------------------------------------
+getChannel | channelName     | get channel object for channelName
+log        | msg channelName | send msg to channelName. If channelName does not exist will default to `client.config.logChannel`
